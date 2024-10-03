2b28835031d50597a96c4e250aa11d8e9a46191b
Abhishek <makizenx7@gmail.com>
2024-10-03 16:10:46 +0530

    Deploy Vercel

--- link-mint-backend/api/index.ts
+++ link-mint-backend/api/index.ts
@@ -1,16 +1,16 @@
 import express, {Request, Response} from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
-import URLrouter from './routes/url'
-import redirectRouter from './routes/redirect'
+import URLrouter from '../src/routes/url'
+import redirectRouter from '../src/routes/redirect'
 import cors from 'cors'
+
 dotenv.config()
 
 const app = express()
 app.use(cors());
-
-
 app.use(express.json())
+
 app.use('/api/url', URLrouter);
 app.use('/',redirectRouter)
 
@@ -25,11 +25,5 @@ app.get('/',(req: Request,res: Response) => {
 })
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT,()=>{
-    console.log('server is running on port',PORT);
-
-})
-
 
 
